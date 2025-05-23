9bdcf6e5585961305203a363a601c74ffcdfd803
SungYeon <yousung1018@naver.com>
2025-05-24 01:51:40 +0900

    ch.9

---
+++ springboot-developer/src/main/java/com/yusungyeon/springbootdeveloper/service/RefreshTokenService.java
@@ -0,0 +1,16 @@
+package com.yusungyeon.springbootdeveloper.service;
+
+import com.yusungyeon.springbootdeveloper.domain.RefreshToken;
+import com.yusungyeon.springbootdeveloper.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
