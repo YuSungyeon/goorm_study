951e41414725342dcf9921fae73a0b3f3ed75a4c
SungYeon <yousung1018@naver.com>
2025-05-23 20:37:19 +0900

    ch.8

---
+++ springboot-developer/src/main/java/com/yusungyeon/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,18 @@
+package com.yusungyeon.springbootdeveloper.service;
+
+import com.yusungyeon.springbootdeveloper.domain.User;
+import com.yusungyeon.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
---
+++ springboot-developer/src/main/java/com/yusungyeon/springbootdeveloper/service/UserService.java
@@ -0,0 +1,22 @@
+package com.yusungyeon.springbootdeveloper.service;
+
+import com.yusungyeon.springbootdeveloper.domain.User;
+import com.yusungyeon.springbootdeveloper.dto.AddUserRequest;
+import com.yusungyeon.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                //패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
